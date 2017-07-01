90751c0d1cc84c40664ea82394d96d10a685ef12
Lorenzo <lferrario@byte-code.com>
2017-07-01 17:27:55 +0200

    rewrite createUser

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require("express");
+// const express = require("express");
 const Web3 = require("web3");
-const identityABI = require("./identityABI");
+const createContract = require("truffle-contract");
 
-const app = express();
-const IdentityAddress = "0x95881d99ed06488e085ce5ed46677a35fdae9a25";
-//getAbi
-// Identity.deployed().then(function(i){return JSON.stringify(i.abi)})
-
+// const app = express();
+let web3;
 if (typeof web3 !== "undefined") {
   web3 = new Web3(web3.currentProvider);
 } else {
   // set the provider you want from Web3.providers
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
-const IdentityContract = web3.eth.contract(identityABI).at(IdentityAddress);
 
-const defaultAccount = web3.eth.accounts[3];
+const identityArtifact = require("./truffle/build/contracts/Identity.json");
+const identityContract = createContract(identityArtifact);
+identityContract.setProvider(web3.currentProvider);
 
 const userName = web3.fromAscii("Pierpaolo");
 const userEmail = web3.fromAscii("dop@pia.pp");
+const payload = { userName, userEmail };
+const sender = web3.eth.accounts[1];
 
 const toAscii = hex => web3.toAscii(hex).replace(/(?:\u0000)+$/, "");
 const convertUser = user => ({
@@ -27,30 +27,51 @@ const convertUser = user => ({
 });
 const convertUserList = userList => userList.map(user => convertUser(user));
 
-IdentityContract.createUser.estimateGas(
-  userName,
-  userEmail,
-  { from: defaultAccount },
-  function(err, result) {
-    if (err) {
-      throw err;
-    } else {
-      var myGasNum = result;
-      IdentityContract.createUser.sendTransaction(
-        userName,
-        userEmail,
-        { from: defaultAccount, gas: myGasNum },
-        function(err, result) {
-          if (err) {
-            throw err;
-          } else {
-            const user = IdentityContract.getUser(defaultAccount);
-            const userList = IdentityContract.getUsers();
-            console.log(convertUser(user));
-            console.log(convertUserList(userList));
-          }
-        }
-      );
-    }
+const createUser = ({ contract, sender, payload: { userName, userEmail } }) => {
+  let instance;
+  return contract
+    .deployed()
+    .then(i => {
+      instance = i;
+      return instance.createUser.estimateGas(userName, userEmail, {
+        from: sender
+      });
+    })
+    .then(gasEstimate =>
+      instance.createUser.sendTransaction(userName, userEmail, {
+        from: sender,
+        gas: gasEstimate
+      })
+    )
+    .then(() => instance.getUser(sender))
+    .then(user => console.log(convertUser(user)))
+    .catch(err => console.log(err));
+};
+
+const asyncCreateUser = async ({
+  contract,
+  sender,
+  payload: { userName, userEmail }
+}) => {
+  try {
+    const instance = await contract.deployed();
+    const gasEstimate = await instance.createUser.estimateGas(
+      userName,
+      userEmail,
+      {
+        from: sender
+      }
+    );
+    await instance.createUser.sendTransaction(userName, userEmail, {
+      from: sender,
+      gas: gasEstimate
+    });
+    const user = await instance.getUser(sender);
+    console.log(convertUser(user));
+    return convertUser(user);
+  } catch (err) {
+    console.log(err);
   }
-);
+};
+
+asyncCreateUser({ contract: identityContract, sender, payload });
